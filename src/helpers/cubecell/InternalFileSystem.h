4e356baa316e3a23e000ab2b5f2bda555dbf47f3
H3wastooshort <git@h3ts.de>
2026-07-18 16:52:11 +0200

    fix: cubecell fs also for other mcu

--- src/helpers/cubecell/InternalFileSystem.h
+++ src/helpers/cubecell/InternalFileSystem.h
@@ -2,20 +2,27 @@
 
 #include <Arduino.h>
 #include "Adafruit_LittleFS.h"
-#define EEPROM_PAGES 3
-#define EEPROM_PAGESIZE CY_FLASH_SIZEOF_ROW
-#define EEPROM_SIZE (EEPROM_PAGESIZE * EEPROM_PAGES)
-#define EEPROM_RWSIZE 8
-#define EEPROM_BASE CY_SFLASH_USERBASE
 
- class InternalFileSystem : public Adafruit_LittleFS
- {
-   public:
-     InternalFileSystem(void);
- 
-     // overwrite to also perform low level format (sector erase of whole flash region)
-     bool begin(void);
- };
- 
- extern InternalFileSystem InternalFS;
+#ifdef __asr650x__
+  #define EEPROM_BASE CY_SFLASH_USERBASE
+  #define EEPROM_PAGES 3
+  #define EEPROM_PAGESIZE CY_FLASH_SIZEOF_ROW
+  #define EEPROM_SIZE (EEPROM_PAGESIZE * EEPROM_PAGES)
+#else
+  #define EEPROM_BASE FLASH_BASE+0x7400
+  #define EEPROM_SIZE 0xC00
+#endif
+
+#define EEPROM_RWSIZE 8
+
+class InternalFileSystem : public Adafruit_LittleFS
+{
+  public:
+    InternalFileSystem(void);
+
+    // overwrite to also perform low level format (sector erase of whole flash region)
+    bool begin(void);
+};
+
+extern InternalFileSystem InternalFS;
  
